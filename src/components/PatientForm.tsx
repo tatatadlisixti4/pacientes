96eebb73818565696f7088c5f9c11fcb11056baf
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-18 18:13:55 -0300

    Definiendo acciones en el store de zustand

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,11 +1,13 @@
 import {useForm} from 'react-hook-form'
 import Error from './Error'
 import type {DraftPatient} from '../types'
+import {usePatientStore} from '../store/store'
 
 export default function PatientForm() {
+    const addPatient = usePatientStore(state => state.addPatient)
     const {register, handleSubmit, formState: {errors}} = useForm<DraftPatient>()
     const registerPatient = (data: DraftPatient) => {
-        console.log(data)
+        addPatient(data)
     }
     // console.log(errors)
     
