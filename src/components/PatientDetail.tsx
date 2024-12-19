3ad517824518f880f675696800e23c3898f880c0
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-18 23:37:11 -0300

    Incorporar redux devtools para inspeccionar el store de zustand

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -8,6 +8,7 @@ type PatientDetailProps = {
 
 export default function PatientDetail({patient} : PatientDetailProps) {
     const deletePatient = usePatientStore(state => state.deletePatient)
+    const getPatientById = usePatientStore(state => state.getPatientById)
     return (
         <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
             <PatientDetailItem label="ID" data={patient.id}/>
@@ -17,10 +18,11 @@ export default function PatientDetail({patient} : PatientDetailProps) {
             <PatientDetailItem label="Fecha Alta" data={patient.date.toString()}/>
             <PatientDetailItem label="Síntomas" data={patient.symptoms}/>
 
-            <div className="flex justify-between mt-10">
+            <div className="flex flex-col lg:flex-row justify-between mt-10 gap-3">
                 <button
                     type="button"
                     className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => getPatientById(patient.id)}
                 >Editar</button>
                 <button
                     type="button"
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,38 @@
 import {create} from "zustand"
+import {devtools} from 'zustand/middleware'
 import {DraftPatient, Patient} from "../types"
 import {v4 as uuidv4} from 'uuid'
 type PatientState = {
     patients: Patient[]
+    activeId: Patient['id']
     addPatient: (data: DraftPatient) => void
-    deletePatient: (id: Patient['id']) => void    
+    deletePatient: (id: Patient['id']) => void  
+    getPatientById: (id: Patient['id']) => void   
 }
 
 const createPatient = (patient: DraftPatient) : Patient => {
     return {...patient, id: uuidv4()}
 }
 
-export const usePatientStore = create<PatientState>((set) => ({
-    patients: [],
-    addPatient: (data) => {
-        const newPatient = createPatient(data)
-        set((state) => ({
-            patients: [...state.patients, newPatient]
-        }))
-    }, 
-    deletePatient: (id) => {
-        set(state => ({
-            patients: state.patients.filter(patient => patient.id !== id)
-        }))
-    }
-}))
+export const usePatientStore = create<PatientState>()(
+    devtools((set) => ({
+        patients: [],
+        activeId: '',
+        addPatient: (data) => {
+            const newPatient = createPatient(data)
+            set((state) => ({
+                patients: [...state.patients, newPatient]
+            }))
+        }, 
+        deletePatient: (id) => {
+            set(state => ({
+                patients: state.patients.filter(patient => patient.id !== id)
+            }))
+        },
+        getPatientById: (id) => {
+            set(() => ({
+                activeId: id
+            }))
+        }
+    })
+))
